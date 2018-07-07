46122d9d3310c543463f945032fb296fa65b2a3f
Shubham Vijay Vargiy <svijay1692@gmail.com>
2018-07-07 15:39:46 +0530

    remove demo

--- index.js
+++ index.js
@@ -52,26 +52,3 @@ const UserExpressApp = class UserExpressApp {
     }
 };
 module.exports = UserExpressApp;
-new UserExpressApp({
-    mail_options: {
-        mail_type: 'sendgrid',
-        sendgrid_options: {
-            api_key: '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'
-        }
-    },
-    model_options: {
-        other_filed: {
-            profile_pic: {
-                type: String,
-                default: ''
-            },
-            name: {
-                type: String,
-                default: ''
-            },
-        }
-    },
-    mongo_options: {
-        mongourl: 'mongodb://localhost:27017/shubh'
-    }
-});
