c52a6f8007157c2f5d18ff4d71aef9bcbe1661a8
Shubham Vijay <svijay1692@gmail.com>
2018-07-09 10:32:11 +0530

    add demo js

---
+++ demo.js
@@ -0,0 +1,23 @@
+new UserExpressApp({
+    mail_options: {
+        mail_type: 'sendgrid',
+        sendgrid_options: {
+            api_key: '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'
+        }
+    },
+    model_options: {
+        other_filed: {
+            profile_pic: {
+                type: String,
+                default: ''
+            },
+            name: {
+                type: String,
+                default: ''
+            },
+        }
+    },
+    mongo_options: {
+        mongourl: 'mongodb://localhost:27017/reactor'
+    }
+});
--- index.js
+++ index.js
@@ -55,27 +55,4 @@ class UserExpressApp {
         private_methods.initialize.call(this, options);
     }
 }
-new UserExpressApp({
-    mail_options: {
-        mail_type: 'sendgrid',
-        sendgrid_options: {
-            api_key: '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'
-        }
-    },
-    model_options: {
-        other_filed: {
-            profile_pic: {
-                type: String,
-                default: ''
-            },
-            name: {
-                type: String,
-                default: ''
-            },
-        }
-    },
-    mongo_options: {
-        mongourl: 'mongodb://localhost:27017/reactor'
-    }
-});
 module.exports = UserExpressApp;
